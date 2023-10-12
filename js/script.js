aaa91a59db94ddc26db2f8fc87cd3fdf6d17c8a6
chiara1911 <chiaracesari89@gmail.com>
2023-10-12 17:35:25 +0200

    add dice console

--- js/script.js
+++ js/script.js
@@ -20,22 +20,40 @@ button.addEventListener('click', function () {
     const email = document.getElementById('mail').value;
     let flag = false;
 
-    for (let email = 0; email <= account.length - 1; email++) {
-        let currentMail = account[email];
-        if (account[email] === currentMail) {       
-            flag = true;                
+    for (let i = 0; i <= account.length - 1; i++) {
+        let currentMail = account[i];
+        if (email === currentMail) {
+            flag = true;
         }
-        
-        if (flag) {
-            text = 'ciao';
-        } else {
-            text = 'via';
     }
-    console.log(currentMail);
-    // console.log(alertEl);
+    if (flag) {
+        text = 'ciao';
+    } else {
+        text = 'via';
+    }
     alertEl.innerHTML = text;
+});
 
-
+// creazione random dadi
+const nome_giocatore = document.querySelector('nome_giocatore');
+    const btn = document.querySelector('.btn-warning');
+    let dadiPlayer= Math.floor((Math.random() * 6) + 1);
+    let dadiCpu = Math.floor((Math.random() * 6) + 1);
+    
+btn.addEventListener('click', function () {
+    if (dadiPlayer > dadiCpu){
+    text = 'hai vinto';
+    } else if (dadiPlayer < dadiCpu){
+        text = 'hai perso';
+    } else {
+        text = 'siete pari';
     }
-}
-)
+    console.log(dadiPlayer);
+    console.log(dadiCpu);
+    console.log(text);
+    
+})
+
+
+
+
